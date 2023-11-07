8c9e2807e937cbb4ca448570fb15bda588f4c75e
Karen Cinca <karencinca1@gmail.com>
2023-11-07 17:30:52 -0300

    api link

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://food-explorer-back-b3j8.onrender.com/',
+    baseURL: 'https://food-explorer-back-b3j8.onrender.com',
     withCredentials: true
 })
